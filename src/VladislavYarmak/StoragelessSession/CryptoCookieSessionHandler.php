8588bd67485b9d33b728def53e8b850b818191c4
Vladislav Yarmak <vladislav@vm-0.com>
2017-04-02 02:03:28 +0300

    backported pbkdf2

--- src/VladislavYarmak/StoragelessSession/CryptoCookieSessionHandler.php
+++ src/VladislavYarmak/StoragelessSession/CryptoCookieSessionHandler.php
@@ -112,7 +112,7 @@ final class CryptoCookieSessionHandler implements \SessionHandlerInterface {
         $iv = substr($message, METADATA_SIZE, $this->cipher_ivlen);
         $ciphertext = substr($message, METADATA_SIZE + $this->cipher_ivlen);
 
-        $key = hash_pbkdf2($this->digest_algo, $this->secret, $id . $valid_till_bin, 1, $this->cipher_keylen, true);
+        $key = $this->pbkdf2($this->digest_algo, $this->secret, $id . $valid_till_bin, 1, $this->cipher_keylen, true);
         $data = openssl_decrypt($ciphertext, $this->cipher_algo, $key, OPENSSL_RAW_DATA, $iv);
         if ($data === false) {
             throw new OpenSSLError();
@@ -128,7 +128,7 @@ final class CryptoCookieSessionHandler implements \SessionHandlerInterface {
         $valid_till_bin = pack(UINT32_LE_PACK_CODE, $expires);
 
         $iv = openssl_random_pseudo_bytes($this->cipher_ivlen);
-        $key = hash_pbkdf2($this->digest_algo, $this->secret, $id . $valid_till_bin, 1, $this->cipher_keylen, true);
+        $key = $this->pbkdf2($this->digest_algo, $this->secret, $id . $valid_till_bin, 1, $this->cipher_keylen, true);
 
         $ciphertext = openssl_encrypt($data, $this->cipher_algo, $key, OPENSSL_RAW_DATA, $iv);
         if ($ciphertext === false) {
@@ -169,6 +169,59 @@ final class CryptoCookieSessionHandler implements \SessionHandlerInterface {
 
     public function gc($maxlifetime) {
         return true;
+    }
+
+    /*
+     * PBKDF2 key derivation function as defined by RSA's PKCS #5: https://www.ietf.org/rfc/rfc2898.txt
+     * $algorithm - The hash algorithm to use. Recommended: SHA256
+     * $password - The password.
+     * $salt - A salt that is unique to the password.
+     * $count - Iteration count. Higher is better, but slower. Recommended: At least 1000.
+     * $key_length - The length of the derived key in bytes.
+     * $raw_output - If true, the key is returned in raw binary format. Hex encoded otherwise.
+     * Returns: A $key_length-byte key derived from the password and salt.
+     *
+     * Test vectors can be found here: https://www.ietf.org/rfc/rfc6070.txt
+     *
+     * This implementation of PBKDF2 was originally created by https://defuse.ca
+     * With improvements by http://www.variations-of-shadow.com
+     */
+    private function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output = false)
+    {
+        $algorithm = strtolower($algorithm);
+        if(!in_array($algorithm, hash_algos(), true))
+            trigger_error('PBKDF2 ERROR: Invalid hash algorithm.', E_USER_ERROR);
+        if($count <= 0 || $key_length <= 0)
+            trigger_error('PBKDF2 ERROR: Invalid parameters.', E_USER_ERROR);
+
+        if (function_exists("hash_pbkdf2")) {
+            // The output length is in NIBBLES (4-bits) if $raw_output is false!
+            if (!$raw_output) {
+                $key_length = $key_length * 2;
+            }
+            return hash_pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output);
+        }
+
+        $hash_length = strlen(hash($algorithm, "", true));
+        $block_count = ceil($key_length / $hash_length);
+
+        $output = "";
+        for($i = 1; $i <= $block_count; $i++) {
+            // $i encoded as 4 bytes, big endian.
+            $last = $salt . pack("N", $i);
+            // first iteration
+            $last = $xorsum = hash_hmac($algorithm, $last, $password, true);
+            // perform the other $count - 1 iterations
+            for ($j = 1; $j < $count; $j++) {
+                $xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
+            }
+            $output .= $xorsum;
+        }
+
+        if($raw_output)
+            return substr($output, 0, $key_length);
+        else
+            return bin2hex(substr($output, 0, $key_length));
     }
 }
 
